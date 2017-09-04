3b0554f40b327f33c2f56bf1ad656b436da5bed6
jamieRowen <jamie@jumpingrivers.com>
2017-09-04 16:53:09 +0100

    updating practicals and data sets to match new notes

---
+++ R/names.R
@@ -0,0 +1,12 @@
+#' Names data
+#'
+#' Number of babies with a given name born in the US for year 2000.
+#'
+#' @name names
+#' @docType data
+#' @usage data(names)
+#' @return A named vector
+#' @keywords datasets
+#' @examples
+#' data(names)
+NULL
---
+++ R/walmartraw.R
@@ -0,0 +1,12 @@
+#' Walmart raw data
+#'
+#' Used only for the graphic example on the slides
+#'
+#'@name walmartraw
+#'@docType data
+#'@usage data(walmartraw)
+#'@return A data frame
+#'@keywords datasets
+#'@examples
+#' data(walmartraw)
+NULL
