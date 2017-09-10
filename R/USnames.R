1d261b6c9fd1375ed869a67efcfb3e9722f342ba
jamieRowen <jamie@jumpingrivers.com>
2017-09-10 11:04:29 +0100

    practicals 3 and 4

---
+++ R/USnames.R
@@ -0,0 +1,12 @@
+#' US baby names
+#'
+#' A collection of names given to children born in the US during the years of 2011-2014
+#'
+#'@name USnames
+#'@docType data
+#'@usage data(USnames)
+#'@return A data frame
+#'@keywords datasets
+#'@examples
+#' data(walmartraw)
+NULL
