731e7a5cee18b8db171159e121e5a14af71ab3a8
Colin Gillespie <csgillespie@gmail.com>
2019-08-16 10:21:17 +0100

    Use static data for Get functions

--- R/Get.R
+++ R/Get.R
@@ -1,34 +1,57 @@
+#' Dummy data
+#'
+#' Dummy data set using in assignment 1
+#'
+#' @name get_data
+#' @docType data
+#' @usage data(get_data, package = "jrIntroduction")
+#' @return A tibble
+#' @keywords datasets
+#' @examples
+#' data(get_data)
+NULL
+
+# nolint start
+# set.seed(seed)
+# l = sample(50000:60000, 1)
+# m = runif(1, -5, 5)
+# v1 = round(rnorm(l, m, 20), 1)
+# v2 = sample(c(TRUE, FALSE), l, replace = TRUE)
+# v3 = sample(LETTERS, l, replace = TRUE)
+#
+# get_data = tibble::tibble(v1, v2, v3)
+# save(get_data, file="data/get_data.RData", version = 2)
+# nolint end
+
+
 #' Generate random vectors
 #'
 #' These functions create vectors of numerics, logicals or characters.
 #' Given a seed, the functions will always return the
 #' same subset.
-#' @inheritParams GetDataFrame
 #' @importFrom stats runif
 #' @importFrom stats rnorm
+#' @importFrom utils data
 #' @export
-GetNumericVector = function(seed=1){
-  set.seed(seed)
-  l = sample(50000:60000, 1)
-  m = runif(1, -5, 5)
-  v = round(rnorm(l, m, 20), 1)
-  return(v)
+GetNumericVector = function() {
+  e = new.env()
+  data("get_data", package = "jrIntroduction", envir = e)
+  e$get_data$v1
 }
 
 #' @export
 #' @rdname GetNumericVector
-GetLogicalVector = function(seed=1){
-  set.seed(seed)
-  l = sample(52000:60000, 1)
-  v = sample(c(TRUE, FALSE), l, replace = TRUE)
-  return(v)
+GetLogicalVector = function() {
+  e = new.env()
+  data("get_data", package = "jrIntroduction", envir = e)
+  e$get_data$v2
+
 }
 
 #' @export
 #' @rdname GetNumericVector
-GetCharacterVector = function(seed=1){
-  set.seed(seed)
-  l = sample(51000:60000, 1)
-  v = sample(LETTERS, l, replace = TRUE)
-  return(v)
+GetCharacterVector = function() {
+  e = new.env()
+  data("get_data", package = "jrIntroduction", envir = e)
+  e$get_data$v3
 }
